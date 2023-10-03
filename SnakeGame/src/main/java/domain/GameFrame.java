25563f4027260dae9a0b83252e2533a98e485d00
Rayh <railande.dasilva@gmail.com>
2023-10-02 21:38:30 -0300

    Snake game using java swing

---
+++ SnakeGame/src/main/java/domain/GameFrame.java
@@ -0,0 +1,16 @@
+package domain;
+
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+
+    public GameFrame(){
+        this.add(new GamePanel());
+        this.setTitle("snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+}
